81ed4e55aa7b9f945927594f91436e24c77a67b6
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-12-19 16:43:11 -0300

    Muestro los resultados de la conexion en la pagina. (list_client.html es la pagina principal)

--- service/client_service.js
+++ service/client_service.js
@@ -1,3 +1,33 @@
+const crearNuevaLinea = (nombre, email, id) => {
+  const linea = document.createElement("tr");
+  const contenido = `
+    <td class="td" data-td>
+      ${nombre}
+    </td>
+    <td>${email}</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/edit_client.html?id=${id}"
+            class="simple-button simple-button--edit"
+          >
+            Editar
+          </a>
+        </li>
+        <li>
+          <button class="simple-button simple-button--delete" type="button" id="${id}">
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  `;
+  linea.innerHTML = contenido;
+  return linea;
+};
+
+const table = document.querySelector("[data-table]");
 const http = new XMLHttpRequest();
 
 http.open("GET", "http://localhost:3000/perfil");
@@ -6,5 +36,7 @@ http.send();
 
 http.onload = () => {
 	const data = JSON.parse(http.response);
-	console.log(data);
+	data.forEach( perfil => {
+		table.appendChild(crearNuevaLinea(perfil.nombre, perfil.email, 0));
+	});
 };
