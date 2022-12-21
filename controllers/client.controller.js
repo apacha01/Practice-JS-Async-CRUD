391e0f4bdb5dee3c60c52867202e75d23c0a355b
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-12-20 21:04:39 -0300

    Cambio identación de archivos.

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,54 +1,55 @@
 import { clientServices } from "../service/client_service.js";
 
 const createNewLine = (name, email, id) => {
-  const line = document.createElement("tr");
-  const content = `
-    <td class="td" data-td>
-      ${name}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/edit_client.html?id=${id}"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button" id="${id}">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  line.innerHTML = content;
+	const line = document.createElement("tr");
+	const content = `
+	<td class="td" data-td>
+		${name}
+	</td>
+	<td>${email}</td>
+	<td>
+		<ul class="table__button-control">
+			<li>
+				<a
+				href="../screens/edit_client.html?id=${id}"
+				class="simple-button simple-button--edit"
+				>
+				Editar
+				</a>
+			</li>
+			<li>
+				<button class="simple-button simple-button--delete" type="button" id="${id}">
+				Eliminar
+				</button>
+			</li>
+		</ul>
+	</td>
+	`;
 
-  const btn = line.querySelector("button");
-  btn.addEventListener("click", () => {
-    const id = btn.id;
-    clientServices
-      .deleteClient(id)
-      .then((response) => {
-        location.reload();
-      })
-      .catch((err) => alert("Hubo un error al eliminar un cliente."));
-  });
+	line.innerHTML = content;
 
-  return line;
-};
+	const btn = line.querySelector("button");
+	btn.addEventListener("click", () => {
+		const id = btn.id;
+		clientServices
+		.deleteClient(id)
+		.then((response) => {
+			location.reload();
+		})
+		.catch((err) => alert("Hubo un error al eliminar un cliente."));
+	});
+
+	return line;
+}
 
 const table = document.querySelector("[data-table]");
 
 clientServices
-  .listClients()
-  .then((data) => {
-    data.forEach( ({ name, email, id} ) => {
-      const newLine = createNewLine(name, email, id);
-      table.appendChild(newLine);
-    });
-  })
-  .catch((error) => alert("Hubo un error al mostrar los clientes."));
+	.listClients()
+	.then((data) => {
+		data.forEach( ({ name, email, id} ) => {
+			const newLine = createNewLine(name, email, id);
+			table.appendChild(newLine);
+		});
+	})
+	.catch((error) => alert("Hubo un error al mostrar los clientes."));
